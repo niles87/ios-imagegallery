c2a046939df9db99be7bb57adf608e862926caab
niles87 <nbing19@gmail.com>
2020-12-09 21:10:10 -0800

    added basic functionality still needs work to show images

--- ImageCapture/CollectionViewController.swift
+++ ImageCapture/CollectionViewController.swift
@@ -5,4 +5,36 @@
 //  Created by Niles Bingham on 12/7/20.
 //
 
-import Foundation
+import UIKit
+
+class CollectionController: UICollectionViewController {
+    
+    var images: [Image] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Image", for: indexPath as IndexPath)
+        let img = UIImage(data: images[indexPath.item].image)
+        let imageView = UIImageView()
+        imageView.image = img
+        cell.contentView.addSubview(imageView)
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("you selected \(indexPath.item)")
+    }
+    
+    func reload() {
+        images = ImageManager.main.getAllImages()
+        self.collectionView.reloadData()
+    }
+}
