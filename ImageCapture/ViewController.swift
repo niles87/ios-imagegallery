dbe3c30fa484ce39439d313fbedc1de6e0de61f3
niles87 <nbing19@gmail.com>
2020-12-12 13:15:41 -0800

    changed code inside view controller to use as a popup

--- ImageCapture/ViewController.swift
+++ ImageCapture/ViewController.swift
@@ -8,41 +8,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var button: UIButton!
+    
+    @IBOutlet var popUp: UIView!
+    
+    var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.backgroundColor = .secondarySystemBackground
-        
-        button.backgroundColor = .systemBlue
-        button.setTitle("Take Picture", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-    }
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        popUp.layer.cornerRadius = 10.0
 
-    @IBAction func didTapButton() {
+        self.showAnimated()
+    }
+        
+    func showAnimated() {
+        self.view.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    func closeAnimated() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+            self.view.alpha = 0.0
+        }, completion: {(finished: Bool) in
+            if (finished) {
+                self.view.removeFromSuperview()
+            }
+        })
+    }
+    
+    @IBAction func closePopUp() {
+        self.closeAnimated()
+    }
+    
+    @IBAction func openCamera() {
         let picker = UIImagePickerController()
         picker.sourceType = .camera
-        picker.allowsEditing = true
+        picker.allowsEditing = false
         picker.delegate = self
-        present(picker, animated: true)
+        present(picker, animated: true, completion: nil)
     }
+    
+    @IBAction func openPhotos() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true, completion: nil)
         
-        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+        guard let photo = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
             return
         }
         
-        imageView.image = image
+        image = photo
+        print(image.size)
     }
 }
